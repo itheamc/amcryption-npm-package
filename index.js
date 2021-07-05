014342786196d2925f71d7dcffdecd534ae7c5c3
Amit Chaudhary <itheamc@gmail.com>
2021-07-05 12:10:01 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * amcryption - a encoding and decoding module
+ * Copyright(c) 2021 @mit <itheamc@gmail.com>
+ * ISC Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/amcryption')
